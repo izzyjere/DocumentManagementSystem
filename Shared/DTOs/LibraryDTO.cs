969fcef65bfaf6ba3bfc349b74eb0f19bf1b8118
Lungdjos <lungudjos@gmail.com>
2023-04-05 12:22:35 +0200

    creating scafford dtos

---
+++ Shared/DTOs/LibraryDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTSADocs.Shared.DTOs
+{
+    internal class LibraryDTO
+    {
+    }
+}
