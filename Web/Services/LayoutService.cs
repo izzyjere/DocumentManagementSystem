b8a176409ca17b8964c3c2d9344b5fe3a984199d
izzyjere <wisdomjere5@gmail.com>
2023-05-14 15:39:58 +0200

    Menu

---
+++ Web/Services/LayoutService.cs
@@ -0,0 +1,9 @@
+﻿using MudBlazor;
+
+namespace RTSADocs.Services
+{
+    public class LayoutService
+    {
+        public static List<BreadcrumbItem> Breadcrumbs {get; set; }  = new List<BreadcrumbItem>();
+    }
+}
