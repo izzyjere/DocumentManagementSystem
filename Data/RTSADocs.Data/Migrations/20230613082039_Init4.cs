1a907356c35c9f91d4c1126eeb5a0c4bbad275b7
izzyjere <wisdomjere5@gmail.com>
2023-06-13 10:35:35 +0200

    File Upload

---
+++ Data/RTSADocs.Data/Migrations/20230613082039_Init4.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RTSADocs.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FileName",
+                table: "PageFile",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "DocumentArchive",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DocumentArchive", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DocumentArchive");
+
+            migrationBuilder.DropColumn(
+                name: "FileName",
+                table: "PageFile");
+        }
+    }
+}
--- Shared/Models/PageFile.cs
+++ Shared/Models/PageFile.cs
@@ -4,6 +4,7 @@
     {
         public string Path { get; set; }
         public string Format { get; set; }
+        public string FileName { get; set; }
         public Document Document { get; set; }
         public DateTime DateUploaded { get; set; }
     }
