c52fcc11ceb2515fcb731fd7bdc47ee8afbcf742
izzyjere <wisdomjere5@gmail.com>
2024-02-06 21:41:51 +0200

    .NET 8

--- Web/Program.cs
+++ Web/Program.cs
@@ -12,52 +12,49 @@ using DMS.Shared.Services;
 
 using SimpleAuthentication;
 
-internal class Program
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new ArgumentNullException("ConnectionString");
+// Add services to the container.
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+builder.Services.AddHangfire(t =>
 {
-    private static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new ArgumentNullException("ConnectionString");
-        // Add services to the container.
-        builder.Services.AddRazorComponents().AddInteractiveServerComponents();
-        builder.Services.AddHangfire(t =>
-        {
-            t.UseSqlServerStorage(connectionString);
-        });
-        builder.Services.AddHangfireServer();
-        builder.Services.AddServerSideBlazor();
-        builder.Services.AddMudServices();
-        builder.Services.AddSimpleAuthentication(userStoreOptions =>
-        {
-            userStoreOptions.UseSqlServer(connectionString);
-        });
-        builder.Services.AddTransient<ICurrentUserService, CurrentUserService>();
-        builder.Services.AddDataAccess(connectionString);
-        builder.Services.AddScoped<IFileSystemService, LocalFileSystemService>();
+    t.UseSqlServerStorage(connectionString);
+});
+builder.Services.AddHangfireServer();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
+builder.Services.AddSimpleAuthentication(userStoreOptions =>
+{
+    userStoreOptions.UseSqlServer(connectionString);
+});
+builder.Services.AddTransient<ICurrentUserService, CurrentUserService>();
+builder.Services.AddDataAccess(connectionString);
+builder.Services.AddScoped<IFileSystemService, LocalFileSystemService>();
 
-        builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
-        var app = builder.Build();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseStaticFiles();
-
-        app.UseRouting();
-        app.UseAuthentication();
-        app.UseAuthorization();
-        app.MigrateDb();
-        app.UseSimpleAuthentication();
-        app.MapBlazorHub();
-        app.UseHangfireDashboard();
-        app.InitFileStoreCleaner();
-        app.Run();
-    }
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+
+app.MigrateDb();
+app.UseSimpleAuthentication();
+app.UseHangfireDashboard();
+app.InitFileStoreCleaner();
+app.Run();
